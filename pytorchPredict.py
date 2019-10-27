89f07afaa35497314e31cfe67e673c09e426844f
qureshi.nomaan <nqafzal@gmail.com>
2019-10-28 02:35:34 +0530

    Added Pytorch Models

---
+++ pytorchPredict.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import random
+import cv2
+from getting_img import get_img
+from tensorflow import keras 
+from skimage import color
+from skimage.transform import resize
+import numpy as np
+import matplotlib.pyplot as plt 
+import os, ssl
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+    getattr(ssl, '_create_unverified_context', None)): 
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 20 * 20, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, 16 * 20 * 20)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+#====================================================================================#
+# Test data importing from keras#
+#====================================================================================#
+data = keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+test_images = test_images/255.0
+train_images = train_images/255.0
+#====================================================================================#
+
+
+#====================================================================================#
+# Loading the trained model. #
+#====================================================================================#
+model = Net()
+model = torch.load("./model.pth")
+for key in model:
+	print(key)
+#====================================================================================#
+
+
+#====================================================================================#
+#This function selects a random image from the test data. #
+#====================================================================================#
+def rands():
+	rand = random.randint(0,10001)
+	prediction = model.predict(test_images[rand][np.newaxis,:,:])
+	plt.grid(False)
+	#print(test_images[rand])
+	plt.imshow(test_images[rand],cmap = plt.cm.binary)
+	plt.title("Prediction : " + str(np.argmax(prediction)))
+	plt.show()
+
+#====================================================================================#
+#This function is for preprocessing the image we get.
+#====================================================================================#
+def preproc(image_data) :
+	res = cv2.cvtColor(image_data, cv2.COLOR_BGR2GRAY)
+	res = resize(res,(28,28), anti_aliasing=True)
+	res = np.rot90(np.fliplr(res))
+	res = (res)*250
+	res = np.around(res)
+	res[res < 20] = 0
+	#print(res)
+	res = res/255
+	return res,res[np.newaxis,:,:]
+#====================================================================================#
+
+#====================================================================================#
+# The loop for detecting drawing an image and predicting what it is. #
+#====================================================================================#
+while True :
+	#rands()
+	image = get_img()
+	image_data = image.get()
+	res,pes = preproc(image_data)
+	prediction = model(pes)
+	plt.grid(False)
+	plt.imshow(res,cmap = plt.cm.binary)
+	print(prediction)
+	plt.title("Prediction : " + str(np.argmax(prediction)))
+	plt.show()
+#====================================================================================#
---
+++ pytorchTrain.py
@@ -0,0 +1,130 @@
+import torch 
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+#=========================================================#
+# Loading the dataset.
+# transforms.ToTensor converts the pil image to image tensor.
+# Channel in pytorch
+	# Example RGB has 3 channels.
+	# Greyscale has 1 channel.
+# transforms.Normalise((m1,m2,m3), (d1, d2, d3))
+# m1, m2, m3 is mean for each channel.
+# d1, d2, d3 is standard deviation. for each channel.
+#=========================================================#
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize([0.5], [0.5])])
+
+trainset = torchvision.datasets.MNIST(
+      root='./data/mnist', 
+			train=True,
+			download=True, 
+			transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, 
+      batch_size=4,
+      shuffle=True, 
+      num_workers=2)
+
+testset = torchvision.datasets.MNIST(
+      root='./data/mnist', 
+      train=False,
+      download=True, 
+      transform=transform)
+
+testloader = torch.utils.data.DataLoader(testset, 
+      batch_size=4,
+      shuffle=False, 
+      num_workers=2)
+
+classes = ('0', '1', '2', '3',
+           '4', '5', '6', '7', '8', '9')
+
+#=========================================================#
+examples = enumerate(trainloader)
+batch_idx, (example_data, example_targets) = next(examples)
+print(example_targets.shape)
+
+# def imshow(img):
+# 	img = img / 2 + 0.5  #unnormalise
+# 	npimg = img.numpy()
+# 	plt.imshow(np.transpose(npimg, (1, 2, 0))) 
+# 	plt.show()
+
+
+# get some random training images
+# dataiter = iter(trainloader)
+# images, labels = dataiter.next()
+
+# # show images
+# # imshow(torchvision.utils.make_grid(images))
+# # print labels
+# print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+#=========================================================#
+
+#=========================================================#
+# Defining the neural network.
+#=========================================================#
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 20 * 20, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = x.view(-1, 16 * 20 * 20)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+#=========================================================#
+
+#=========================================================#
+# Defining the Neural Network Loss Function. 
+#=========================================================#
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum = 0.9)
+#=========================================================#
+
+#=========================================================#
+# Training the model. 
+#=========================================================#
+for epoch in range(2):
+  running_loss = 0.0
+  for i, data in enumerate(trainloader):
+    inputs, label = data
+    optimizer.zero_grad()
+    outputs = net(inputs)
+    loss = criterion(outputs, label)
+    loss.backward()
+    optimizer.step()
+    running_loss += loss.item()
+    if i % 2000 == 1999:    # print every 2000 mini-batches
+        print('[%d, %5d] loss: %.3f' %
+        (epoch + 1, i + 1, running_loss / 2000))
+        running_loss = 0.0
+#=========================================================#
+
+
+
+#=========================================================#
+# Saving the Pytorch Model.
+#=========================================================#
+PATH = './model.pth'
+torch.save(net.state_dict(), PATH)
+#=========================================================#
+
+
